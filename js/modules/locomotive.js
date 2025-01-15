99e730aebb8b0a23a371b707dff06df13b1e212b
mugilavathi <you@example.com>
2025-01-15 18:31:39 +0530

    template put

---
+++ js/modules/locomotive.js
@@ -0,0 +1,16 @@
+
+// init LocomotiveScroll on page load
+export const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  lerp: 0.085,
+  multiplier: 0.85,
+  scrollFromAnywhere: true,
+  tablet: {
+    smooth: false,
+  },
+  smartphone: { smooth: false },
+});
+setTimeout(() => {
+  scroll.update();
+}, 500);
